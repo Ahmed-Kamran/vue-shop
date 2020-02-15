bb7c7adee8c9ff7ba11a63419009c1c906902e3a
Ahmed Rimon <55627047+Ahmed-Rimon@users.noreply.github.com>
2020-02-15 19:16:57 +0600

    stripe api settings

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,45 @@
 const functions = require('firebase-functions');
-
+var stripe = require('stripe')('sk_test_qVEhLnftslEW8bq2KEmHM7WR00ryT3a06v');
+const cors = require('cors')({
+    origin: true
+});
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+
+// exports.Hello = functions.https.onRequest((request, response) => {
+//     // response.send("Hello: " + request.query.lastName);
+//     if (request.method == 'POST') {
+//         response.send("Hello: " + request.query.lastName)
+//     } else {
+//         response.send("Please Send Post Request");
+//     }
 // });
+
+exports.CheckoutSession = functions.https.onRequest((request, response) => {
+
+    cors(request, response, () => {
+        stripe.checkout.sessions.create({
+                success_url: 'https://example.com/success',
+                cancel_url: 'https://example.com/cancel',
+                payment_method_types: ['card'],
+                line_items: [{
+                    name: 'T-shirt',
+                    description: 'Comfortable cotton t-shirt',
+                    amount: 1500,
+                    currency: 'usd',
+                    quantity: 2,
+                }, ],
+            },
+
+            function(err, session) {
+                // asynchronously called
+                response.send(session);
+            }
+        );
+    });
+
+});
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -26,95 +26,134 @@
           </ul>
         </div>
         <div class="col-md-4">
-          <form action="/charge" method="post" id="payment-form">
+          <p>Total Price : {{ this.$store.getters.totalPrice | currency }}</p>
+
+          <button class="btn btn-primary" @click="pay">Checkout</button>
+          <!-- <form action="/charge" method="post" id="payment-form">
             <div class="form-row">
               <label for="card-element">Credit or debit card</label>
-              <div id="card-element">
-                <!-- A Stripe Element will be inserted here. -->
-              </div>
+          <div id="card-element">-->
+          <!-- A Stripe Element will be inserted here. -->
+          <!-- </div> -->
 
-              <!-- Used to display form errors. -->
-              <div id="card-errors" role="alert"></div>
+          <!-- Used to display form errors. -->
+          <!-- <div id="card-errors" role="alert"></div>
             </div>
 
             <button>Submit Payment</button>
-          </form>
+          </form>-->
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-// Create a Stripe client.
-var stripe = Stripe("pk_test_dbcNbOvmNphdIdhrC7c2jsz900Jw8Sp6OI");
+// import { Card, createToken } from "vue-stripe-elements-plus";
+import axios from "axios";
+// const Stripe = require("stripe");
+var stripe = Stripe("pk_test_r1ukNyqyCq8ko0o6S2TzEyHT00WOnd8xF3");
+export default {
+  data() {
+    return {
+      sessionId: ""
+    };
+  },
 
-// Create an instance of Elements.
-var elements = stripe.elements();
-
-// Custom styling can be passed to options when creating an Element.
-// (Note that this demo uses a wider set of styles than the guide below.)
-var style = {
-  base: {
-    color: "#32325d",
-    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-    fontSmoothing: "antialiased",
-    fontSize: "16px",
-    "::placeholder": {
-      color: "#aab7c4"
+  methods: {
+    pay() {
+      stripe
+        .redirectToCheckout({
+          // sessionId: "{{CHECKOUT_SESSION_ID}}"
+          sessionId: this.sessionId.id
+        })
+        .then(function(result) {});
     }
   },
-  invalid: {
-    color: "#fa755a",
-    iconColor: "#fa755a"
+
+  created() {
+    axios
+      .post("http://localhost:5000/vue-shop-ebb44/us-central1/CheckoutSession")
+      .then(response => {
+        this.sessionId = response.data;
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 };
 
-// Create an instance of the card Element.
-var card = elements.create("card", { style: style });
+// // Create a Stripe client.
+// var stripe = Stripe("pk_test_dbcNbOvmNphdIdhrC7c2jsz900Jw8Sp6OI");
 
-// Add an instance of the card Element into the `card-element` <div>.
-card.mount("#card-element");
+// // Create an instance of Elements.
+// var elements = stripe.elements();
 
-// Handle real-time validation errors from the card Element.
-card.addEventListener("change", function(event) {
-  var displayError = document.getElementById("card-errors");
-  if (event.error) {
-    displayError.textContent = event.error.message;
-  } else {
-    displayError.textContent = "";
-  }
-});
+// // Custom styling can be passed to options when creating an Element.
+// // (Note that this demo uses a wider set of styles than the guide below.)
+// var style = {
+//   base: {
+//     color: "#32325d",
+//     fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+//     fontSmoothing: "antialiased",
+//     fontSize: "16px",
+//     "::placeholder": {
+//       color: "#aab7c4"
+//     }
+//   },
+//   invalid: {
+//     color: "#fa755a",
+//     iconColor: "#fa755a"
+//   }
+// };
 
-// Handle form submission.
-var form = document.getElementById("payment-form");
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
+// // Create an instance of the card Element.
+// var card = elements.create("card", { style: style });
 
-  stripe.createToken(card).then(function(result) {
-    if (result.error) {
-      // Inform the user if there was an error.
-      var errorElement = document.getElementById("card-errors");
-      errorElement.textContent = result.error.message;
-    } else {
-      // Send the token to your server.
-      stripeTokenHandler(result.token);
-    }
-  });
-});
+// // Add an instance of the card Element into the `card-element` <div>.
+// card.mount("#card-element");
 
-// Submit the form with the token ID.
-function stripeTokenHandler(token) {
-  // Insert the token ID into the form so it gets submitted to the server
-  var form = document.getElementById("payment-form");
-  var hiddenInput = document.createElement("input");
-  hiddenInput.setAttribute("type", "hidden");
-  hiddenInput.setAttribute("name", "stripeToken");
-  hiddenInput.setAttribute("value", token.id);
-  form.appendChild(hiddenInput);
+// // Handle real-time validation errors from the card Element.
+// card.addEventListener("change", function(event) {
+//   var displayError = document.getElementById("card-errors");
+//   if (event.error) {
+//     displayError.textContent = event.error.message;
+//   } else {
+//     displayError.textContent = "";
+//   }
+// });
 
-  // Submit the form
-  form.submit();
-}
+// // Handle form submission.
+// var form = document.getElementById("payment-form");
+// form.addEventListener("submit", function(event) {
+//   event.preventDefault();
+
+//   stripe.createToken(card).then(function(result) {
+//     if (result.error) {
+//       // Inform the user if there was an error.
+//       var errorElement = document.getElementById("card-errors");
+//       errorElement.textContent = result.error.message;
+//     } else {
+//       // Send the token to your server.
+//       stripeTokenHandler(result.token);
+//     }
+//   });
+// });
+
+// // Submit the form with the token ID.
+// function stripeTokenHandler(token) {
+//   // Insert the token ID into the form so it gets submitted to the server
+//   var form = document.getElementById("payment-form");
+//   var hiddenInput = document.createElement("input");
+//   hiddenInput.setAttribute("type", "hidden");
+//   hiddenInput.setAttribute("name", "stripeToken");
+//   hiddenInput.setAttribute("value", token.id);
+//   form.appendChild(hiddenInput);
+
+//   // Submit the form
+//   form.submit();
+// }
+//
 </script>
 <style scoped lang="scss">
 .del_item {
